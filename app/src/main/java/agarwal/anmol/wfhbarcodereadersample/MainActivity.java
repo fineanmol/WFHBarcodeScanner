fc8f5695903d4f9bfefa44c5d741af455b7f8076
Anmol Agarwal <anmol.agarwal2004@yahoo.com>
2017-10-27 14:22:05 +0530

    Working Image capture with Solved Storage Issue Barcode Scanner Works fine Error- integration to server part

--- app/src/main/java/agarwal/anmol/wfhbarcodereadersample/MainActivity.java
+++ app/src/main/java/agarwal/anmol/wfhbarcodereadersample/MainActivity.java
@@ -1,12 +1,14 @@
 package agarwal.anmol.wfhbarcodereadersample;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.Image;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,15 +22,28 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.barcode.Barcode;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.lang.*;
 
 import agarwal.anmol.wfhbarcodereader.WfhBarcodeScanner;
 
@@ -38,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     final int REQ_CODE = 12;
 
     TextView result;
-    Button scanButton, scanDialogButton , scanImage;
+    Button scanButton, scanDialogButton, scanImage ,btnPost;
     EditText barcodeTypes;
     Spinner modeSpinner;
     ImageView imageHolder;
@@ -88,22 +103,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tempArray = items.toArray(tempArray);
         barcodeTypeItems = tempArray;
     }
-
+    Person person;
     boolean[] checkedStates = new boolean[TYPE_MAP.size()];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // we are going to use asynctask to prevent network on main thread exception
+        // new PostDataAsyncTask().execute();
         result = (TextView) findViewById(R.id.result);
         scanButton = (Button) findViewById(R.id.scan);
         scanDialogButton = (Button) findViewById(R.id.scan_dialog);
         barcodeTypes = (EditText) findViewById(R.id.barcode_types);
         modeSpinner = (Spinner) findViewById(R.id.scanner_mode);
-        scanImage=(Button)findViewById(R.id.photo_button);
-        imageHolder=(ImageView)findViewById(R.id.captured_photo);
-        
+        scanImage = (Button) findViewById(R.id.photo_button);
+        imageHolder = (ImageView) findViewById(R.id.captured_photo);
+        btnPost = (Button)findViewById(R.id.btnupload);
+        // add click listener to Button "POST"
+        btnPost.setOnClickListener(this);
+
+
 
         ArrayAdapter adapter = new SpinnerAdapter(this, R.layout.simple_spinner_item);
         modeSpinner.setAdapter(adapter);
@@ -138,20 +158,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         scanImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               openApp(MainActivity.this, "agarwal.anmol.wfhcameraclick");
-
+                openApp(MainActivity.this, "agarwal.anmol.wfhcameraclick");
             }
 
 
         });
-
     }
 
-    public static boolean openApp(Context context , String packageName)
-    {
+    public static boolean openApp(Context context, String packageName) {
         PackageManager manager = context.getPackageManager();
-        Intent i= manager.getLaunchIntentForPackage(packageName);
-        if (i==null) {
+        Intent i = manager.getLaunchIntentForPackage(packageName);
+        if (i == null) {
             return false;
             //throw new packageManager.NameNotFoundException();
         }
@@ -173,20 +190,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //startActivity(intent);
         } else if (view.getId() == barcodeTypes.getId()) {
             showBarcodeTypesPicker();
-        }
-        else if(view.getId() == scanDialogButton.getId()) {
+        } else if (view.getId() == scanDialogButton.getId()) {
             ScannerDialog dialog = ScannerDialog.instantiate(mMode, new ScannerDialog.DialogResultListener() {
                 @Override
                 public void onScanned(Barcode... barcode) {
-                    if(barcode.length > 1) {
+                    if (barcode.length > 1) {
                         mBarcode = null;
                         mBarcodes = new ArrayList<Barcode>();
 
-                        for(Barcode b:barcode){
+                        for (Barcode b : barcode) {
                             mBarcodes.add(b);
                         }
-                    }
-                    else {
+                    } else {
                         mBarcodes = null;
                         mBarcode = barcode[0];
                     }
@@ -203,6 +218,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.d("MAIN", "showing scanner dialog");
             dialog.show(getSupportFragmentManager(), "SCANNER");
         }
+
+
+ /*       switch(view.getId()){
+            case R.id.btnupload:
+                if(!validate())
+                    Toast.makeText(getBaseContext(), "Enter some data!", Toast.LENGTH_LONG).show();
+                // call AsynTask to perform network operation on separate thread
+                new HttpAsyncTask().execute("http://hmkcode.appspot.com/jsonservlet");
+                break;
+    }
+    */
     }
 
     void showBarcodeTypesPicker() {
@@ -226,7 +252,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .show();
     }
 
-    @WfhBarcodeScanner.BarCodeFormat int[] getUpdatedFormats() {
+    @WfhBarcodeScanner.BarCodeFormat
+    int[] getUpdatedFormats() {
         List<Integer> formats = new ArrayList<>();
         String barcodes = "";
         int count = 0;
@@ -240,6 +267,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }
+
 
         if (count > 0) {
             @WfhBarcodeScanner.BarCodeFormat int[] iFormats = new int[formats.size()];
@@ -371,4 +399,110 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return view;
         }
     }
+
+
+ /*   public static String POST(String url, Person person){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+          //  HttpClientConnection httpclient = new DefaultBHttpClientConnection(url);
+
+           HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("name", result.getName());
+
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+
+            person.setName(result.getText().toString());
+
+
+            return POST(urls[0],person);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean validate(){
+        if(result.getText().toString().trim().equals(""))
+            return false;
+
+        else
+            return true;
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+*/
+
 }
+
